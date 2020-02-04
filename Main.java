977cf4482424ded440d55036733569288ec7a5eb
bethportela <60664976+bethportela@users.noreply.github.com>
2020-02-04 16:26:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		int area, perimetro;
-		int base = 7, altura = 5;
+		
+		System.out.println("Base: ");
+		int base = sc.nextInt();
+		System.out.println("Altura: ");
+		int altura = sc.nextInt();
+		sc.close();
 		
 		area = base*altura;
 		perimetro = 2*base + 2*altura;
-		System.out.println("A area é: "+ area+ " e o perimetro: "+ perimetro+".");
+		
+		System.out.println("Area: "+area);
+		System.out.println("Perimetro: "+perimetro);
 	}
 }

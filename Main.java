99183ebf1bdc84202d897c912b57e1e7b932b8d0
bethportela <60664976+bethportela@users.noreply.github.com>
2020-02-04 16:02:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int area, perimetro;
+		int base = 7, altura = 5;
+		
+		area = base*altura;
+		perimetro = 2*base + 2*altura;
+		System.out.println("A area é: "+ area+ " e o perimetro: "+ perimetro+".");
+	}
+}

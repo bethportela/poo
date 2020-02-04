aac3419bbdb4676eaa4bc55b495f1c00664bccea
bethportela <60664976+bethportela@users.noreply.github.com>
2020-02-04 14:40:06 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o nome do aluno: ");
+		String nome = sc.nextLine();
+		System.out.println("Inofrme a idade do aluno: ");
+		int idade = sc.nextInt();
+		sc.close();
+		
+		System.out.println("Aluno "+nome+ ", sua idade é "+idade+ " anos.");
+	}
+
+}
